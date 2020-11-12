c6e80c21b047edaeb6c3505cb478737ccc9c564e
m0odero <56940448+m0odero@users.noreply.github.com>
2020-11-12 19:14:59 +0200

    Add files via upload

---
+++ labs/lab02/modero_lab02_stereopair.py
@@ -0,0 +1,37 @@
+#Importing the necessary libraries
+import sys
+import PIL
+import numpy as np
+import pandas as pd
+from PIL import Image
+from scipy import signal
+
+# provide three arguments: the name of the left_image.jpg, 
+# right_image.jpg, and the name of the padded_image.jpg
+if ( len( sys.argv ) != 4 ):
+    print( "usage is: %s image_in image_out" % (sys.argv[0]) )
+    print( "        image_in is the distorted image" )
+    print( "        image_out is the processed image" )
+    sys.exit()
+    
+# load the left and right image
+left_im         = Image.open( sys.argv[1] )
+left_im_arr     = np.asarray( left_im, np.float )
+left_resized    = left_im.resize( (2048, 1020) )
+
+right_im        = Image.open( sys.argv[2] )
+right_im_arr    = np.asarray( right_im, np.float )
+right_resized   = right_im.resize( (2048, 1020) )
+
+# creating the black  image for stereoscope
+rows            = 4096
+cols            = 4096
+black_array     = np.zeros( [ rows, cols, 3 ], dtype=np.uint8 )
+black_array.fill(0)
+black           = Image.fromarray( black_array )
+
+#paste
+black.paste( left_resized, (1024, 514) )
+black.paste( right_resized, (1024, 2562) )
+black.save( sys.argv[3]  )
+black.show()
